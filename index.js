859fdd3db456e6645b7497629ee03a26c49bb548
guigen <guillaume.gence@gmail.com>
2018-11-25 21:41:12 +0100

    Affichage

--- index.js
+++ index.js
@@ -1,54 +1,104 @@
-let Depense = {
-  prix: 0,
-  payeurs : []
+
+var body = d3.select("body");
+let afficher = function (text) {
+  body.append("p").text(text)
+}
+
+//Une part c'est un peu comme une action. Tu les achêtes à un prix et tout peut prendre de la valeur
+//Pour vendre des parts il faut convenir dans les status de la procédure (A l'hunamité ou non)
+let Part = {
+  valeurNominal : 1, //En euros
+  valeurReel : 1 //En euros
+}
+
+let Associe = function (nom,apportNumInitial = 0,sci){
+
+  let nbPart = apportNumInitial;
+
+  let getNom = function () {
+    return nom;
+  }
+
+  let getApportInitial = function () {
+    return apportNumInitial;
+  }
+
+  let getNbPart = function () {
+    return nbPart;
+  }
+
+  let effectuerUnApport = function (valeurDeLapport) {
+    nbPart += valeurDeLapport;
+    afficher(nom + " fait un apport de "+ valeurDeLapport + " € ", " son nombre de part est maintenant de "+nbPart);
+    sci.update();
+  }
+
+  return {
+    getNom,
+    getApportInitial,
+    getNbPart,
+    effectuerUnApport
+  }
 }
 
 
 let sci = function() {
     let nbPartTotal = 0;
+    let associes = [];
 
-    getNbPartTotal = function () {
-      console.log("Le nombre de part dans la SCI est de "+nbPartTotal);
+    let getNbPartTotal = function () {
+      afficher("Il y a " + nbPartTotal + " parts dans la sci.");
       return nbPartTotal;
     }
 
+    let getValeurReelTotal = function () {
+      afficher("Il y a " + nbPartTotal + " parts dans la sci, pour une valeur estimé de "+(nbPartTotal * Part.valeurReel)+ " €");
+      return nbPartTotal * Part.prixPartReel;
+    }
+
+
+    let ajouterAssocie = function (associe) {
+      associes.push(associe);
+      afficher(associe.getNom() + " a été ajouté à la SCI, avec un apport initial de " + associe.getApportInitial() + "€");
+      update()
+    }
+
+    let update = function (){
+      const reducer = (accumulator, currentValue) => accumulator + currentValue.getNbPart();
+      nbPartTotal = associes.reduce(reducer,0);
+    }
+
     return {
-      getNbPartTotal : getNbPartTotal
+      getNbPartTotal,
+      getValeurReelTotal,
+      ajouterAssocie,
+      update
     }
 
 }
 
-let Associe = function (){
-  let nom = "";
-  let nbPart = 0;
-
-  var acheter = function () {
-
-  }
-
-  return {
-    acheter : acheter
-  }
-}
 
 
-
-
-
-//Une part c'est un peu comme une action. Tu les achêtes à un prix et tout peut prendre de la valeur
-//Pour vendre des parts il faut convenir dans les status de la procédure (A l'hunamité ou non)
-let Part = {
-  valeurNominal : 1,
-  valeurReel : 1
-}
 
 let main = function () {
 
   let SCI_LFQCEG = sci();
   SCI_LFQCEG.getNbPartTotal();
 
-  let Leo = Associe();
-  let Fio = Associe();
-  let Guillaume = Associe();
+
+  let Leo = Associe("Léo",17500,SCI_LFQCEG);
+  SCI_LFQCEG.ajouterAssocie(Leo);
+
+  let Fio = Associe("Fio",17500,SCI_LFQCEG);
+  SCI_LFQCEG.ajouterAssocie(Fio);
+
+  let Guillaume = Associe("Guillaume",17500,SCI_LFQCEG);
+  SCI_LFQCEG.ajouterAssocie(Guillaume);
+
+  Guillaume.effectuerUnApport(40);
+  Fio.effectuerUnApport(500);
+
+  SCI_LFQCEG.getValeurReelTotal();
+
 
 }()
