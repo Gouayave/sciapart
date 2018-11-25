09523d8cc287ec68eed8a6f785dc63b18c4b5ea0
Gui <>
2018-11-25 15:00:13 +0100

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+let Associe = {
+  let nom = "";
+  let nbPart = 0;
+
+  return {
+    creer : creer,
+    vendre : 
+  }
+}
+
+
+let Depense = {
+  var prix = 0;
+  var payeurs = [
+
+  ]
+}
+
+
+//Une part c'est un peu comme une action. Tu les achêtes à un prix et tout peut prendre de la valeur
+//Pour vendre des parts
+let Part = {
+  const valeurNominal = 1;
+  var valeurReel = 1;
+}
